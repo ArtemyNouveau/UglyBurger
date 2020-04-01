d4f35e2f7a8e92005772858f729a105564b539bc
Артем <artnovkov@gmail.com>
2020-04-01 16:48:57 +0300

    perfomance improve

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,29 @@
-import React, {Fragment} from "react";
+import React, {Component, Fragment} from "react";
 import BackDrop from '../BackDrop/BackDrop'
 
 import styles from './Modal.module.css'
 
-const modal = (props) => (
-    <Fragment>
-        <BackDrop show={props.show} close={props.closeModal}/>
-        <div
-            className={styles.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0',
-            }}
-        >
-            {props.children}
-        </div>
-    </Fragment>
-)
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal
+    render () {
+        return (
+            <Fragment>
+                <BackDrop show={this.props.show} close={this.props.closeModal}/>
+                <div
+                    className={styles.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0',
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Modal
